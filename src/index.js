cd9bb911e990eec7d221e51287efdcbf8bc005f1
Hyeonjong <nianelo4@gmail.com>
2018-12-18 00:35:48 +0900

    Use default application markup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import '@mdi/font/scss/materialdesignicons.scss';
+import 'vuetify/dist/vuetify.min.css';
+import MyApp from '@/my-app';
+
+Vue.use(Vuetify, {
+    iconfont: 'mdi',
+});
 
 new Vue({
     el: '#app',
+    components: {
+        MyApp,
+    },
+    render(h) {
+        return h(MyApp);
+    },
     mounted() {
         this.$nextTick(() => {
             console.log('Hello Vuetify!');
---
+++ src/my-app.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+        <v-navigation-drawer app></v-navigation-drawer>
+        <v-toolbar app></v-toolbar>
+        <v-content>
+            <v-container fluid>
+                <p>Hello Vuetify!</p>
+            </v-container>
+        </v-content>
+        <v-footer app></v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'my-app',
+};
+</script>
