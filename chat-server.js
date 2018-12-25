18d4668e81fadd9acf96f9d6b5305855aa3d9520
Hyeonjong <nianelo4@gmail.com>
2018-12-25 16:35:54 +0900

    Add chat server class

---
+++ chat-server.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+class ChatServer {
+  constructor(portNumber = 3000) {
+    io.on('connection', (socket) => {
+      console.log('A user connected');
+    });
+
+    http.listen(portNumber, () => {
+      console.log(`Listening on localhost:${portNumber}`);
+    });
+  }
+}
+
+module.exports = ChatServer;
